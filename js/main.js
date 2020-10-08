56be842a3e412c71ec7b957bce6b420b8076d6fa
Francis-Bota <francis.bota@amalitech.org>
2020-10-08 14:59:13 +0000

    Second Commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 $(document).ready(function (){
- 
+    $('#button-create-item').on('click',function(){
+        let name = $('#input-create-item').val();
+        $('#input-create-item').val('');
+
+        let html = '';
+        html += '<div class="item">'
+        html +=  '<div class="name">' + name + '</div>'
+        html +=  '<img src="assets/beach.jpg"></img>'
+        html +=  '<div class="description">Wallpaper</div>'
+        html +=  '<div class="price">$49.99</div>'
+        html += ' <button>Add to Cart</button>'
+        html += '</div>'
+
+        $('#container').prepend(html);
+    });
+
+
 });
